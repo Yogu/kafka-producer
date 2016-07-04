86682a9afdc29b55bd103a6af4d5cc25991e79f7
Jan Melcher <mail@jan-melcher.de>
2016-07-04 15:11:28 +0200

    Add library for kafka consumer/producer

--- src/main/java/ProducerMain.java
+++ src/main/java/ProducerMain.java
@@ -1,130 +1,29 @@
-import com.siemens.ct.exi.EXIFactory;
-import com.siemens.ct.exi.GrammarFactory;
-import com.siemens.ct.exi.api.sax.EXIResult;
-import com.siemens.ct.exi.api.sax.EXISource;
-import com.siemens.ct.exi.grammars.Grammars;
-import com.siemens.ct.exi.helpers.DefaultEXIFactory;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.w3c.dom.Document;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.sax.SAXSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.*;
-import java.net.URL;
-import java.util.Properties;
+import de.unistuttgart.isw.serviceorchestration.api.MessageBus;
+import de.unistuttgart.isw.serviceorchestration.api.MessageSender;
 
 public class ProducerMain {
 
-    static final String xmlFile = "<?xml vesion=\"1.0\" encoding=\"utf-8\"?>\n" +
-            "<ListOfFloat xmlns=\"http://opcfoundation.org/UA/2008/02/Types.xsd\">\n" +
-            "    <Float>3.14</Float>\n" +
-            "    <Float>42.0</Float>\n" +
-            "    <Float>2.78</Float>\n" +
-            "    <Float>2.81e-13</Float>\n" +
-            "    <Float>NaN</Float>\n" +
-            "    <Float+Inf>\n" +
-            "</ListOfFloat>";
-    static final String EXI_EXTENSION = "_exi";
-    static final String EXI_SCHEMA_EXTENSION = "_exi_schema";
-    static final String XML_EXTENSION = ".xml";
+	static final String xmlFile = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+			"<ListOfFloat xmlns=\"http://opcfoundation.org/UA/2008/02/Types.xsd\">\n" +
+			"    <Float>3.14</Float>\n" +
+			"    <Float>42.0</Float>\n" +
+			"    <Float>2.78</Float>\n" +
+			"    <Float>2.81e-13</Float>\n" +
+			"    <Float>NaN</Float>\n" +
+			"</ListOfFloat>";
 
+	public static void main(String[] args) throws Exception {
+		// opcfoundation does not like Java default user agent
+		System.setProperty("http.agent", "Mozilla/5.0");
 
-    private String getEXILocation() {
+		MessageBus bus = new MessageBus();
+		MessageSender sender = bus.createSender("output", "https://opcfoundation.org/UA/2008/02/Types.xsd");
 
-        return "/home/martin/Dokumente/test" + "Test" + EXI_SCHEMA_EXTENSION;
+		while (true) {
+			sender.send(xmlFile);
+			Thread.sleep(1000);
+			System.out.println("Sent message");
+		}
+	}
 
-    }
-
-    protected void encode(String xml, ContentHandler ch) throws Exception {
-        XMLReader xmlReader = XMLReaderFactory.createXMLReader();
-        xmlReader.setContentHandler(ch);
-
-        // parse xml file
-        xmlReader.parse(new InputSource(new StringReader(xml)));
-    }
-
-    protected void decode(XMLReader exiReader, String exiLocation)
-            throws SAXException, IOException, TransformerException {
-
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-
-        InputStream exiIS = new FileInputStream(exiLocation);
-        SAXSource exiSource = new SAXSource(new InputSource(exiIS));
-        exiSource.setXMLReader(exiReader);
-
-        OutputStream os = new FileOutputStream(exiLocation + XML_EXTENSION);
-        transformer.transform(exiSource, new StreamResult(os));
-        os.close();
-    }
-
-    protected void codeSchemaInformed() throws Exception {
-        String exiLocation = getEXILocation();
-
-        // create default factory and EXI grammar for schema
-        EXIFactory exiFactory = DefaultEXIFactory.newInstance();
-        GrammarFactory grammarFactory = GrammarFactory.newInstance();
-        Grammars g = grammarFactory.createGrammars(new URL("https://opcfoundation.org/UA/2008/02/Types.xsd").openStream());
-        exiFactory.setGrammars(g);
-
-        // encode
-        OutputStream exiOS = new FileOutputStream(exiLocation);
-        EXIResult exiResult = new EXIResult(exiFactory);
-        exiResult.setOutputStream(exiOS);
-        encode(xmlFile, exiResult.getHandler());
-        exiOS.close();
-
-        // decode
-        EXISource saxSource = new EXISource(exiFactory);
-        XMLReader xmlReader = saxSource.getXMLReader();
-        decode(xmlReader, exiLocation);
-    }
-
-    public static void main(String[] args) throws Exception {
-        ProducerMain main =  new ProducerMain();
-        main.codeSchemaInformed();
-
-//        // XML
-//        String xmlLocation;
-//        String xmlName;
-//        // XML Schema
-//        String xsdLocation;
-//
-//        String xml = " <xs:element name=\"Float\" nillable=\"true\" type=\"xs:float\" />";
-//
-//        Properties props = new Properties();
-//        props.put("bootstrap.servers", "kafka:9092");
-//        props.put("acks", "all");
-//        props.put("retries", 0);
-//        props.put("batch.size", 16384);
-//        props.put("linger.ms", 1);
-//        props.put("buffer.memory", 33554432);
-//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-//
-//        String topicName = System.getenv("OUTPUT_output");
-//        System.out.println("Using topic " + topicName);
-//
-//        System.out.println("Creating producer...");
-//        Producer<String, String> producer = new KafkaProducer<>(props);
-//        System.out.println("Created producer. Pushing message stream");
-//        int i = 0;
-//        while (true) {
-//            producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(i),
-//                    Integer.toString(i)));
-//            i++;
-//        }
-
-    }
 }
---
+++ src/main/java/de/unistuttgart/isw/serviceorchestration/api/MessageHandler.java
@@ -0,0 +1,8 @@
+package de.unistuttgart.isw.serviceorchestration.api;
+
+/**
+ * Created by jan on 7/4/16.
+ */
+public interface MessageHandler {
+	void handleMessage(String xml);
+}
